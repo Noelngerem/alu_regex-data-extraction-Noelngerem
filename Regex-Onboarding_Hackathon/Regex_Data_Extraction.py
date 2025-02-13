f40d6e4a69102b46315ea11a6d544eac158bca23
Noelngerem <e.ngerem@alustudent.com>
2025-02-13 03:44:03 +0100

    Regex - Onboarding Hackathon

--- Regex-Onboarding_Hackathon/Regex_Data_Extraction.py
+++ Regex-Onboarding_Hackathon/Regex_Data_Extraction.py
@@ -5,17 +5,17 @@ def extract_emails(data):
     return re.findall(regex_for_email, data)
 
 def extract_urls(data):
-    regex_for_urls = r"https?://(?:www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:/[a-zA-Z0-9./?=&%-]*)?"
+    regex_for_urls = r"https?://(?:www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:/[a-zA-Z0-9./?=&%#-]*)?"
     return re.findall(regex_for_urls, data)
 
 def extract_phone_numbers(data):
-    regex_for_phone_numbers = r"\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}"
+    regex_for_phone_numbers = r"\+?\d{1,3}[\s.-]?\(?\d{2,4}\)?[\s.-]?\d{2,4}[\s.-]?\d{2,4}[\s.-]?\d{0,4}"
     return re.findall(regex_for_phone_numbers, data)
 
 def extract_credit_cards(data):
-    regex_for_credit_cards = r"\b(?:\d{4}[- ]?){3}\d{4}\b"
+    regex_for_credit_cards = r"\b(?:\d{4}[- ]?){3}\d{4}|\b\d{4}[- ]?\d{6}[- ]?\d{5}\b"
     return re.findall(regex_for_credit_cards, data)
 
 def extract_time(data):
-    regex_for_time = r"\b(?:[01]?\d|2[0-3]):[0-5]\d\b|\b(?:1[0-2]|0?[1-9]):[0-5]\d\s?(?i)(?:AM|PM)\b"
+    regex_for_time = r"\b(?:[01]?\d|2[0-3]):[0-5]\d(?:\s?(?i)(?:AM|PM))?\b"
     return re.findall(regex_for_time, data)
